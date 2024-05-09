f98a2adecf867e0e628d593ae40ad9044bd4be18
朱乾力 <zhu.qianli@darcytech.com>
2024-05-09 13:38:06 +0800

    feat extractUniqueKey

--- src/main/java/com/zhuqianli/devtoolkit/utils/PsiAnnotationUtils.java
+++ src/main/java/com/zhuqianli/devtoolkit/utils/PsiAnnotationUtils.java
@@ -4,6 +4,7 @@ import com.intellij.lang.jvm.annotation.JvmAnnotationAttribute;
 import com.intellij.lang.jvm.annotation.JvmAnnotationConstantValue;
 import com.intellij.psi.*;
 import com.zhuqianli.devtoolkit.java.lang.JavaClass;
+import org.apache.commons.compress.utils.Lists;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
@@ -80,24 +81,25 @@ public class PsiAnnotationUtils {
      * @return ["userId", "tid"]
      */
     public static List<String> extractUniqueKey(JavaClass psiClass) {
-        PsiAnnotation tableAnnotation = psiClass.getAnnotation("javax.persistence.Table");
-        if (tableAnnotation == null) {
-            return Collections.emptyList();
-        }
-        PsiAnnotationMemberValue uniqueConstraints = tableAnnotation.findAttributeValue("uniqueConstraints");
-        if (uniqueConstraints instanceof PsiAnnotation) {
-            PsiAnnotationMemberValue columnNames = ((PsiAnnotation) uniqueConstraints).findAttributeValue("columnNames");
-            if (columnNames == null) {
-                return Collections.emptyList();
-            }
-            PsiAnnotationMemberValue[] columnNamesValues = ((PsiArrayInitializerMemberValue) columnNames).getInitializers();
-            return Arrays.stream(columnNamesValues).map(columnNamesValue -> {
-                PsiLiteralExpression expression = (PsiLiteralExpression) columnNamesValue;
-                return Objects.requireNonNull(expression.getValue()).toString();
-            }).collect(Collectors.toList());
-        } else {
-            return Collections.emptyList();
-        }
+//        PsiAnnotation tableAnnotation = psiClass.getAnnotation("javax.persistence.Table");
+//        if (tableAnnotation == null) {
+//            return Collections.emptyList();
+//        }
+//        PsiAnnotationMemberValue uniqueConstraints = tableAnnotation.findAttributeValue("uniqueConstraints");
+//        if (uniqueConstraints instanceof PsiAnnotation) {
+//            PsiAnnotationMemberValue columnNames = ((PsiAnnotation) uniqueConstraints).findAttributeValue("columnNames");
+//            if (columnNames == null) {
+//                return Collections.emptyList();
+//            }
+//            PsiAnnotationMemberValue[] columnNamesValues = ((PsiArrayInitializerMemberValue) columnNames).getInitializers();
+//            return Arrays.stream(columnNamesValues).map(columnNamesValue -> {
+//                PsiLiteralExpression expression = (PsiLiteralExpression) columnNamesValue;
+//                return Objects.requireNonNull(expression.getValue()).toString();
+//            }).collect(Collectors.toList());
+//        } else {
+//            return Collections.emptyList();
+//        }
+        return Lists.newArrayList();
     }
 
     @Nullable
